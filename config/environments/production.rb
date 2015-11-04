50fa934e8b5286f375268f4ab95da512438426ce
Chris Siegman <chrissiegman@yahoo.com>
2015-11-04 22:01:21 +0000

    Fix host url in smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'sheltered-island-5311.herokuapp.com'
+  host = 'https://sheltered-island-5311.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :addresses        => 'smtp.sendgrid.net',
